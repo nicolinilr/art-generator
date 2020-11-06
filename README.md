a62899babbfaa208240f301203debcd1e380af86
Chemslayer <nicolinilr@gmail.com>
2020-11-06 12:09:28 -0800

    Add README and create source arrays for generator

---
+++ README.md
@@ -0,0 +1,22 @@
+#Random Art Generator
+
+A simple app to generate a random art description, using Javascript
+
+##Table of Contents
+
+* [General Info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+
+##General Info
+This project is a simple art description generator. This was developed as a freeform project guided by Codecademy, and the idea for an art generator is inspired by the game Rimworld.
+
+##Technologies
+Project is created with:
+* Javascript
+* NodeJS v10.19.0
+
+##Setup
+To run this project, install Node locally and run with `node artGenerator.js`
+
---
+++ artGenerator.js
@@ -0,0 +1,11 @@
+//Arrays containing strings to insert into the final art description
+const singularNouns = ['cat','dog','bike','door','dragon','bank','advanced component','Javascript developer', 'chair', 'car', 'spaceship' ];
+const adjectives = ['red', 'angry', 'loud','blue','large','small','excited','sheepish','giant','rowdy','bored','poor','steady'];
+const verbs = ['petting','shooting','riding','spotting','driving','eating','obeying','fighting','arm wrestling','igniting','programming'];
+
+
+
+
+
+
+//This artwork depicts [a/#][adjective(s)][nouns(s)] [verb-ing] [a/#][adjective(s)][nouns(s)]. 
